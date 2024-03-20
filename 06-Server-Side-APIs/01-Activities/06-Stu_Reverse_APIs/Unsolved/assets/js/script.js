98ad937c573d60748d11d414e21045093d5da6ca
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-03-19 20:24:44 -0400

    functions definition

--- 06-Server-Side-APIs/01-Activities/06-Stu_Reverse_APIs/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/06-Stu_Reverse_APIs/Unsolved/assets/js/script.js
@@ -2,6 +2,7 @@ var requestUrl = 'https://api.github.com/orgs/Netflix/repos';
 
 // JQuery AJAX
 // TODO: Comment on how AJAX returns an API call
+  // AJAX returns an API call by using the $.ajax method to make a request to the server and then returns a promise that resolves to the Response to that request, whether it is successful or not.
 $.ajax({
   url: requestUrl,
   method: 'GET',
@@ -12,6 +13,7 @@ $.ajax({
 
 // Browser Fetch Method
 // TODO: Comment on how Fetch returns an API call
+  // fetch returns an API call by using the fetch method to make a request to the server and then returns a promise that resolves to the Response to that request, whether it is successful or not.
 fetch(requestUrl)
   .then(function (response) {
     return response.json();
@@ -23,6 +25,7 @@ fetch(requestUrl)
 
 // Browser XMLHttpRequest
 // TODO: Comment on how XMLHttpRequest returns an API call
+  // XMLHttpRequest returns an API call by using the XMLHttpRequest object to interact with servers. You can retrieve data from a URL without having to do a full page refresh. This enables a Web page to update just part of a page without disrupting what the user is doing.
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
   if (xhr.readyState === XMLHttpRequest.DONE) {
@@ -34,3 +37,4 @@ xhr.open('GET', requestUrl);
 xhr.send();
 
 // TODO: Comment on the differences on the format of the data that was returned
+  // The data returned from the AJAX and Fetch methods are in JSON format, while the data returned from the XMLHttpRequest method is in string format.
