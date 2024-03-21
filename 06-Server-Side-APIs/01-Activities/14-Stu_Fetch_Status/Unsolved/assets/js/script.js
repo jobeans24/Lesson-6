4b5611578c3597d7c8e594dece747541dcdb6c97
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-03-21 19:23:02 -0400

    march 21

--- 06-Server-Side-APIs/01-Activities/14-Stu_Fetch_Status/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/14-Stu_Fetch_Status/Unsolved/assets/js/script.js
@@ -8,6 +8,11 @@ function getApi(requestUrl) {
       // Check the console first to see the response.status
       console.log(response.status);
       // Then write the conditional based on that response.status value
+      if (response.status === 200) {
+        responseText.textContent = 'Status: ' + response.status;
+      } else {
+        responseText.textContent = 'Status: ' + response.status;
+      }
       // Make sure to display the response on the page
     })
     .then(function (data) {
