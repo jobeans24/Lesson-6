993237467f54be5a4fd1f33c47ddc16853ad4c30
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-03-21 20:49:31 -0400

    mar 21

--- 06-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
@@ -1,7 +1,16 @@
 var badRequestUrl = 'https://api.github.com/unicorns';
 var redirectUrl = './404.html';
 
+
 fetch(badRequestUrl).then(function (response) {
   // Use a conditional to check the response status.
-  // If that status equals the conditional, then redirect to the 404 page.
+ if (response.status === 404) {
+    // If that status equals the conditional, then redirect to the 404 page.
+    document.location.href = redirectUrl;
+  } else {
+    // If the status doesn't equal the conditional, then return the response.
+    return response.json();
+  }
 });
+
+
